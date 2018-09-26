a7cea13ecc3a29916d930a8b65c216780d729a77
Shane Scott <shaneradio@gmail.com>
2018-09-26 18:43:36 -0400

    adding gme objects to SSnake.js

--- SSnake.js
+++ SSnake.js
@@ -1,3 +1,11 @@
 let ctx = document.getElementById("ctx").getContext("2d");
 
-ctx.fillText("test test test", 150, 250);
+// ctx.fillText("test test test", 150, 250);
+
+let WIDTH = 500;
+let HEIGHT = 500;
+ctx.font = "20px Helvetica";
+
+let snakeObj = {
+    
+}
