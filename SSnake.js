e7a95addf344311c7314228b46ffaeccad0b56ab
Shane Scott <shaneradio@gmail.com>
2018-10-19 11:13:54 -0400

    comments in SSnake.js

--- SSnake.js
+++ SSnake.js
@@ -1,3 +1,4 @@
+
 let ctx = document.getElementById("ctx").getContext("2d");
 
 // ctx.fillText("test test test", 150, 250);
@@ -11,9 +12,10 @@ let direction;
 
 ctx.font = "20px Helvetica";
 
+// let snakeBody
 let snakeObj = {
     // snakeObj to be repeated as segments of
-    //multi-part snake graphic
+    // multi-part snake graphic
     width: 20,
     height: 20,
     color: "green"
@@ -40,6 +42,7 @@ document.onkeydown = function (event) {
     }
 }
 
+// let food
 let foodObj = {
     width: 20,
     height: 20,
@@ -66,12 +69,13 @@ const drawFood = function (fObj, i) {
 }
 
 const startGame = function () {
+    // snakeList
     snakeItems = [
         {x:220, y:200},
         {x:210, y:200},
         {x:200, y:200}
     ];
-
+    // foodList
     foodItems = [];
     direction = 99;
     snakeItems.forEach(drawSnake);
